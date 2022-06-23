ffb91acc93829088dbbba396cfd0d4fdc23caf10
YashAgarwal44 <77672385+YashAgarwal44@users.noreply.github.com>
2022-06-23 16:47:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,21 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  canvas =  createCanvas(700, 600);
+  canvas.parent("canvas");
+  canvas.center();
+
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, ModelLoaded);
+  poseNet.on("pose", gotPoses);
 }
 
+function modelLoaded(){
+  console.log("Model Is Loaded");
+}
 
 function draw(){
 
